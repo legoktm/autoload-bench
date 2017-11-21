1494eae06f9c8414d52532c99fc02f46b791b401
Kunal Mehta <legoktm@member.fsf.org>
2017-11-20 20:43:08 -0800

    Use current composer's ClassLoader instead of an outdated copy
    Override the loadClass() function to prevent actual loading of the file

--- src/Seld/AutoloadBench/Loader/Composer.php
+++ src/Seld/AutoloadBench/Loader/Composer.php
@@ -2,125 +2,23 @@
 
 namespace Seld\AutoloadBench\Loader;
 
-/*
- * Copied from Composer sources (MIT licensed)
- *
- * (c) Nils Adermann <naderman@naderman.de>
- *     Jordi Boggiano <j.boggiano@seld.be>
- *
- * This class is loosely based on the Symfony UniversalClassLoader.
- *
- * @author Fabien Potencier <fabien@symfony.com>
- * @author Jordi Boggiano <j.boggiano@seld.be>
- */
-class Composer
-{
-    private $prefixes = array();
-    private $fallbackDirs = array();
-    private $useIncludePath = false;
-    private $classMap = array();
+use Composer\Autoload\ClassLoader;
 
-    /**
-     * @param array $classMap Class to filename map
-     */
-    public function addClassMap(array $classMap)
-    {
-        if ($this->classMap) {
-            $this->classMap = array_merge($this->classMap, $classMap);
-        } else {
-            $this->classMap = $classMap;
-        }
-    }
+class Composer extends ClassLoader {
+	/**
+	 * Override to avoid actually loading the class
+	 *
+	 * @param string $class
+	 *
+	 * @return bool
+	 */
+	public function loadClass($class)
+	{
+		if ($file = $this->findFile($class)) {
+			return true;
+		}
 
-    /**
-     * Registers a set of classes
-     *
-     * @param string       $prefix The classes prefix
-     * @param array|string $paths  The location(s) of the classes
-     */
-    public function add($prefix, $paths)
-    {
-        if (!$prefix) {
-            foreach ((array) $paths as $path) {
-                $this->fallbackDirs[] = $path;
-            }
+		return false;
+	}
 
-            return;
-        }
-        if (isset($this->prefixes[$prefix])) {
-            $this->prefixes[$prefix] = array_merge(
-                $this->prefixes[$prefix],
-                (array) $paths
-            );
-        } else {
-            $this->prefixes[$prefix] = (array) $paths;
-        }
-    }
-
-    /**
-     * Loads the given class or interface.
-     *
-     * @param  string       $class The name of the class
-     * @return Boolean|null True, if loaded
-     */
-    public function loadClass($class)
-    {
-        if ($file = $this->findFile($class)) {
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * Finds the path to the file where the class is defined.
-     *
-     * @param string $class The name of the class
-     *
-     * @return string|null The path, if found
-     */
-    public function findFile($class)
-    {
-        if (isset($this->classMap[$class])) {
-            return $this->classMap[$class];
-        }
-
-        if ('\\' == $class[0]) {
-            $class = substr($class, 1);
-        }
-
-        if (false !== $pos = strrpos($class, '\\')) {
-            // namespaced class name
-            $classPath = str_replace('\\', DIRECTORY_SEPARATOR, substr($class, 0, $pos)) . DIRECTORY_SEPARATOR;
-            $className = substr($class, $pos + 1);
-        } else {
-            // PEAR-like class name
-            $classPath = null;
-            $className = $class;
-        }
-
-        $classPath .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-
-        foreach ($this->prefixes as $prefix => $dirs) {
-            if (0 === strpos($class, $prefix)) {
-                foreach ($dirs as $dir) {
-                    if (file_exists($dir . DIRECTORY_SEPARATOR . $classPath)) {
-                        return $dir . DIRECTORY_SEPARATOR . $classPath;
-                    }
-                }
-            }
-        }
-
-        foreach ($this->fallbackDirs as $dir) {
-            if (file_exists($dir . DIRECTORY_SEPARATOR . $classPath)) {
-                return $dir . DIRECTORY_SEPARATOR . $classPath;
-            }
-        }
-
-        if ($this->useIncludePath && $file = stream_resolve_include_path($classPath)) {
-            return $file;
-        }
-
-        $this->classMap[$class] = false;
-    }
 }
